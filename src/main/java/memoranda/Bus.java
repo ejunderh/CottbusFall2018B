d6533cde2cf1317c26519b995ccc6bf2dd8cfdc6
afiorentino <ajfioren@asu.edu>
2018-10-31 19:20:50 -0700

    completed bus class and added documentation

--- src/main/java/memoranda/Bus.java
+++ src/main/java/memoranda/Bus.java
@@ -1,10 +1,51 @@
+/**
+ * Bus class representing a bus for the Jerra program
+ */
 public class Bus {
-private int busID;
-private int seats;
+    
+    private int busID;
+    private int seats;
 
-public Bus();
+    /**
+     * Constructor for the bus class
+     * @param busID the id for the bus
+     * @param seats the number of seats on the bus
+     */
+    
+    public Bus(int busID, int seats){
+        this.busID = busID;
+        this.seats = seats;
+    }
+    
+    /**
+     * @return busID the id for the bus
+     */
+     
+    public int getBusID(){
+        return this.busID;
+    }
+    
+    /**
+     * @param newBusID the new id for the bus
+     */
 
-public Bus(int busID, int seats){
-    this.busID = busID;
-    this.seats = seats;
+    public void setBusID(int newBusID){
+        this.busID = newBusID;
+    }
+
+    /**
+     * @return seats the number of seats on the bus
+     */
+    
+    public int getSeats(){
+        return this.seats;
+    }
+
+    /**
+     * @param newSeats the number of seats on the bus
+     * /
+    
+    public void setSeats(int newSeats){
+        this.seats = newSeats;
+    }
 }
